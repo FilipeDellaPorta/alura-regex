f695aef8b3606d76666de6d35cf4363aa2b21924
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-10-29 18:28:05 -0300

    usando âncoras para filtrar a busca por data

--- buscaData.js
+++ buscaData.js
@@ -5,3 +5,7 @@ const banco = fs.readFileSync(bancoCsv, "utf-8");
 const patternData = /\d{2}[\/. ]?\d{2}[\/. ]?\d{4}$/gm;
 const matchData = banco.match(patternData);
 console.log(matchData);
+
+const somenteAno = /\d{4}$/gm;
+const macthAno = banco.match(somenteAno);
+console.log(macthAno);
