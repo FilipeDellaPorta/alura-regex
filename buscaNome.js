95595ca81e74526bce472836fd4b9477717e565e
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-10-30 20:53:48 -0300

    pattern regex alterantivo para capturar nomes intermediários

--- buscaNome.js
+++ buscaNome.js
@@ -3,6 +3,7 @@ const bancoCsv = "database.csv";
 const banco = fs.readFileSync(bancoCsv, "utf-8");
 
 const patternNomes = /^([A-Za-zÀ-ÿ]+)(?:\s([A-Za-zÀ-ÿ]+))+/gm;
+//^([A-Za-zÀ-ÿ]+)((\s[A-Za-zÀ-ÿ]+)+) alternativa para capturas nomes intermediários
 
 const matchNomes = banco.match(patternNomes);
 console.log(`Todos os nomes:`, matchNomes);
