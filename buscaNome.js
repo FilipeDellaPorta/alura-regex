b9c8b3e59a23eeeb30b99ccc5101db2e3178a1d2
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-10-30 20:44:23 -0300

    usando âncora para que capture o padrão quando ocorrer no começo da linha

--- buscaNome.js
+++ buscaNome.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const bancoCsv = "database.csv";
 const banco = fs.readFileSync(bancoCsv, "utf-8");
 
-const patternNomes = /([A-Za-zÀ-ÿ]+)(\s[A-Za-zÀ-ÿ]+)+/g;
+const patternNomes = /^([A-Za-zÀ-ÿ]+)(\s[A-Za-zÀ-ÿ]+)+/gm;
 
 const matchNomes = banco.match(patternNomes);
 console.log(`Todos os nomes:`, matchNomes);
