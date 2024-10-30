20476f67f349a707bffddf2982994d7d7b99210d
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-10-30 20:33:19 -0300

    usando busca de caracteres acentuados para lista de nomes

---
+++ buscaNome.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const bancoCsv = "database.csv";
+const banco = fs.readFileSync(bancoCsv, "utf-8");
+
+const patternNomes = /[A-Za-zÀ-ÿ]+\s[A-Za-zÀ-ÿ]+/g;
+
+const matchNomes = banco.match(patternNomes);
+console.log(`Todos os nomes:`, matchNomes);
